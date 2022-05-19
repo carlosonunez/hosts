cbfa549e86f313baaa00c3cdb18666a751af2e62
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2022-05-19 14:16:26 -0500

    release 1.1.0!
    Welcome to v1.1.0 of `hosts`. We extended on `sources.yaml` to
    generalize the files that we're creating and the exclusions that apply
    to them. This makes creating different permutations of these files
    eaiser in the future.
    
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- build_hosts.sh
+++ build_hosts.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 #vi: set ft=bash:
-DOCKER_IMAGE_NAME="briceburg/yq"
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-HOSTS_WITH_FACEBOOK="$SCRIPT_DIR/hosts/with-fb.txt"
-HOSTS_WITHOUT_FACEBOOK="$SCRIPT_DIR/hosts/without-fb.txt"
+FILES_DIR="${FILES_DIR:-$SCRIPT_DIR}"
 SOURCES_FILE="$SCRIPT_DIR/sources.yaml"
-EXCLUSIONS_FILE="$SCRIPT_DIR/exclusions"
-ALL_FILES="${HOSTS_WITH_FACEBOOK},${HOSTS_WITHOUT_FACEBOOK}"
 GNU_SED_CONFIRMED=0
-
-export DOCKER_DEFAULT_PLATFORM='linux/amd64'
 
 gnu_sed() {
   # if sed errors when running --version then it's probably the BSD variant.
@@ -31,19 +25,22 @@ fail() {
   exit 1
 }
 
-create_host_dirs() {
-  for file in $(tr ',' '\n' <<< "$ALL_FILES")
+hosts_file_path() {
+  echo "${FILES_DIR}/hosts/${1}.txt"
+}
+
+create_host_files_and_dirs() {
+  for file in $1
   do
-    test -f "$file" && rm -f "$file"
-    test -d "$(dirname "$file")" || mkdir -p "$(dirname "$file")"
-    touch "$file"
+    fp=$(hosts_file_path "$1")
+    dir=$(dirname "$fp")
+    test -f "$fp" && rm -f "$fp"
+    (mkdir -p "$dir" && touch "$fp") || fail "[$fp] Couldn't create ths file; see logs"
   done
 }
 
 gather_sources() {
-  docker run --rm -v "$SOURCES_FILE:/sources.yaml" "$DOCKER_IMAGE_NAME" \
-    -r '.sources[] | .name + "%" + .url + "%" + (.blocks_fb|tostring)' '/sources.yaml' | \
-    tr -d $'\r'
+  yq -r '.sources[] | .name' "$SOURCES_FILE"
 }
 
 process_whitelist() {
@@ -52,7 +49,7 @@ process_whitelist() {
   do
     while read -r pattern
     do
-      >&2 echo "===> Removing from $file: $pattern"
+      >&2 echo "Removing from $file: $pattern"
       gnu_sed -Ei "/$pattern/d" "$file"
     done < <(grep -E "$match_re" "$file")
     gnu_sed -Ei "/^$/d; /^#/d; /^[ ]+#/d" "$file"
@@ -60,21 +57,77 @@ process_whitelist() {
   done
 }
 
-create_host_dirs
-
-while read -r source
-do
-  name=$(cut -f1 -d '%' <<< "$source")
-  url=$(cut -f2 -d '%' <<< "$source")
-  blocks_fb=$(cut -f3 -d '%' <<< "$source")
-  >&2 echo "===> Fetching hosts from source: $name"
-  hosts=$(curl -sSL "$url")
-  echo "$hosts" >> "$HOSTS_WITH_FACEBOOK"
-  if grep -Eiq '^false$' <<< "$blocks_fb"
+write_hosts_files() {
+  src="$1"
+  files="$2"
+  data=$(yq --arg source "$src" -r \
+    '.sources[] | select(.name == $source) | .name + "%" + .url + "%" +
+      (if .exclude_files != null then (.exclude_files | join(",")) else "none" end)' "$SOURCES_FILE")
+  if test -z "$data" || grep 'null' <<< "$data"
   then
-    echo "$hosts" >> "$HOSTS_WITHOUT_FACEBOOK"
+    fail "[$src] Unable to find one or more fields"
   fi
+  name="$(cut -f1 -d '%' <<< "$data")"
+  url="$(cut -f2 -d '%' <<< "$data")"
+  files_to_skip=$(cut -f3 -d '%' <<< "$data")
+  >&2 echo "[$name] Downloading domains from '$url'..."
+  domains=$(curl -sL "$url") || fail "[$name] Failed to download, or source has no content"
+  for file in $files
+  do
+    if ! grep -q "$file" <<< "$files_to_skip"
+    then
+      >&2 echo "[$name] Writing to '$(hosts_file_path "$file")'..."
+      cat >>"$(hosts_file_path "$file")" <<< "$domains"
+    fi
+  done
+}
 
-done < <(gather_sources)
-process_whitelist &&
->&2 echo "===> Host files are ready"
+sort_and_remove_duplicates() {
+  for file in $files
+  do
+    fp=$(hosts_file_path "$file")
+    orig_count=$(wc -l "$fp" | cut -f1 -d ' ')
+    >&2 echo "[$file] Sorting and removing dupes..."
+    sort -ruo "$fp" "$fp"
+    final_count=$(wc -l "$fp" | cut -f1 -d ' ')
+    diff=$(echo "${final_count}-${orig_count}" | bc)
+    >&2 echo "[$file] $diff duplicate domains removed (orig: $orig_count, final: $final_count)"
+  done
+}
+
+process_whitelists() {
+  files="$1"
+  for file in $files
+  do
+    regexps=""
+    whitelists="$(yq -r --arg file "$file" \
+      '.files[] | select(.name == $file) | .whitelists[]' "$SOURCES_FILE")"
+    for whitelist in $whitelists
+    do
+      patterns=$(yq -r --arg whitelist "$whitelist" \
+        '.whitelists[] | select(.name == $whitelist) | .patterns | join("%")' "$SOURCES_FILE")
+      pattern_count=$(tr '%' '\n' <<< "$patterns" | wc -l)
+      >&2 echo "[$file] Adding $pattern_count patterns from whitelist: $whitelist"
+      regexps="$regexps%$patterns"
+    done
+    match_re="0.0.0.0 ($(sed 's/^%//; s#%#\|#g; s/.$//g' <<< "$regexps"))$"
+    while read -r pattern
+    do
+      >&2 echo "[$file] Removing from this file: $pattern"
+    done < <(grep -E "$match_re" "$(hosts_file_path "$file")")
+  done
+}
+
+gather_files() {
+  yq -r '.files[].name' "$SOURCES_FILE"
+}
+
+files=$(gather_files)
+create_host_files_and_dirs $files
+gather_sources |
+  while read -r src
+  do write_hosts_files "$src" "$files"
+  done
+sort_and_remove_duplicates "$files"
+process_whitelists "$files"
+>&2 echo "[core] Script complete"
